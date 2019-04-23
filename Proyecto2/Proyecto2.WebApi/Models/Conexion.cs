6951c2d052f76041ce9d8d71bb59e1f3e532a2ea
Ever <everchicasp@gmail.com>
2019-04-23 00:45:35 -0600

    Agregue la conexion a la base de datos en Amazon

--- Proyecto2/Proyecto2.WebApi/Models/Conexion.cs
+++ Proyecto2/Proyecto2.WebApi/Models/Conexion.cs
@@ -8,13 +8,28 @@ namespace Proyecto2.WebApi.Models
 {
     public class Conexion
     {
+        /// <summary>
+        /// Clase que retorna la cadena de conexion a la base de datos
+        /// </summary>
+        /// <returns></returns>
         public static string CadenaConexion()
         {
             MySqlConnectionStringBuilder conn = new MySqlConnectionStringBuilder();
+
+            /*
             conn.Server = "localhost";
             conn.Database = "proyecto2bases1";
             conn.UserID = "root";
             conn.Password = "chicas";
+            */
+
+            // USAR ESTA SI LA PRIMERA NO FUNCIONA
+            //conn.Server = "proyecto2bases1.curgdovy7biz.us-east-2.rds.amazonaws.com";
+            
+            conn.Server = "proyecto2.curgdovy7biz.us-east-2.rds.amazonaws.com";
+            conn.Database = "Proyecto2Bases1";
+            conn.UserID = "proyecto2bases1";
+            conn.Password = "proyecto2bases1";
             conn.IntegratedSecurity = false;
             return conn.ToString();
         }
