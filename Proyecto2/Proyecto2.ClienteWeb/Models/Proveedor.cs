783cae99e58690ed8a2dda31cfb4d579908a91a5
Ever <everchicasp@gmail.com>
2019-05-05 15:37:02 -0600

    Procedimiento de login, y el login funcional

--- Proyecto2/Proyecto2.ClienteWeb/Models/Proveedor.cs
+++ Proyecto2/Proyecto2.ClienteWeb/Models/Proveedor.cs
@@ -7,7 +7,7 @@ namespace Proyecto2.ClienteWeb.Models
 {
     public class Proveedor
     {
-        ublic int proveedor { get; set; }
+        public int proveedor { get; set; }
         public string Nombre { get; set; }
         public string Direccion { get; set; }
         public string Correo { get; set; }
