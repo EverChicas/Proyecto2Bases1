a2885be7b37324dea71ed19d02e30729183a7707
Brandon Alvarez <brandonalvarez94@hotmail.com>
2019-05-05 17:17:16 -0600

    * Login con administrador * Pagina administrador * Cerrar Sesion

---
+++ Proyecto2/Proyecto2.ClienteWeb/Controllers/CerrarSesionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto2.ClienteWeb.Models;
+namespace Proyecto2.ClienteWeb.Controllers
+{
+    public class CerrarSesionController : Controller
+    {
+        public ActionResult vFinalizado()
+        {
+            Usuario usuario = Session["USUARIO"] as Usuario;
+            Session["USUARIO"] = null;
+            return RedirectToAction("vInicio", "Home");
+        }
+    }
+}
