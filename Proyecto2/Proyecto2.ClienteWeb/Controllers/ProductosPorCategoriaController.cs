c4650e9bdd890f52af67547d0d02cc6595bc290d
Brandon Alvarez <brandonalvarez94@hotmail.com>
2019-05-07 14:45:10 -0600

    Asignar y ver productos por categoría, procedimientos agregados

---
+++ Proyecto2/Proyecto2.ClienteWeb/Controllers/ProductosPorCategoriaController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Newtonsoft.Json;
+using Proyecto2.ClienteWeb.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto2.ClienteWeb.Controllers
+{
+    public class ProductosPorCategoriaController : Controller
+    {
+        // GET: ProductosPorCategoria
+        public ActionResult vProductosPorCategoria()
+        {
+            return View();
+        }
+
+
+        public ActionResult mostrandoProductos(string tipo_categoria)
+        {
+            IEnumerable<Producto> listado = Lista(tipo_categoria);
+            Session["PRODUCTOS_POR_CATEGORIA"] = listado;
+            return RedirectToAction("vProductosPorCategoria", "ProductosPorCategoria");   
+        }
+
+        public IEnumerable<Producto> Lista(string tipo_categoria)
+        {
+            var url = "http://localhost:61291/api/ProductosPorCategoria?";
+            string action = string.Format("tipo_categoria={0}", tipo_categoria);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url + action);
+            HttpResponseMessage response = HttpInstance.GetHttpClientInstance().SendAsync(request).Result;
+            List<Producto> lista = new List<Producto>();
+            if(response.IsSuccessStatusCode)
+            {
+                var responsecontent = response.Content.ReadAsStringAsync().Result;
+                var listado = JsonConvert.DeserializeObject<List<Producto>>(responsecontent.ToString());
+                lista = listado;
+            }
+            return lista;
+        }
+    }
+}
---
+++ Proyecto2/Proyecto2.WebApi/Controllers/ProductosPorCategoriaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySql.Data.MySqlClient;
+using Proyecto2.WebApi.Models;
+
+namespace Proyecto2.WebApi.Controllers
+{
+    public class ProductosPorCategoriaController : ApiController
+    {
+        [HttpPost]
+        public IEnumerable<Producto> Get(string tipo_categoria)
+        {
+            List<Producto> lista = new List<Producto>();
+            MySqlConnection conection = new MySqlConnection(Conexion.CadenaConexion());
+            conection.Open();
+            MySqlCommand command = new MySqlCommand("PRODUCTOS_POR_CATEGORIA", conection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@NOMBRE_CATEGORIA", tipo_categoria);
+            MySqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                lista.Add(new Producto(int.Parse(reader.GetValue(0).ToString()), reader.GetValue(1).ToString(), double.Parse(reader.GetValue(2).ToString())));
+            }
+            conection.Close();
+            return lista;
+        }
+    }
+}
