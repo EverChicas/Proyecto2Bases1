adc892067ee97eb9a8f189103fe6439d40f93893
Brandon Alvarez <brandonalvarez94@hotmail.com>
2019-05-06 21:18:03 -0600

    * Crear tipo_categoria, Crear producto (ambas con su pagina respectiva)

---
+++ Proyecto2/Proyecto2.ClienteWeb/Controllers/NuevoProductoController.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Proyecto2.ClienteWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto2.ClienteWeb.Controllers
+{
+    public class NuevoProductoController : Controller
+    {
+        // GET: NuevoProducto
+        public ActionResult vNuevoProducto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult creandoNuevoProducto(string nombre, double precio)
+        {
+            Usuario userLogueado = Session["USUARIO"] as Usuario;
+            var url = "http://localhost:61291/api/NuevoProducto?";
+            string action = string.Format("nombre={0}&precio={1}", nombre, precio);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url + action);
+            HttpResponseMessage response = HttpInstance.GetHttpClientInstance().SendAsync(request).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var responsecontent = response.Content.ReadAsStringAsync().Result;
+                var agregado = JsonConvert.DeserializeObject<Boolean>(responsecontent.ToString());
+                if (agregado)
+                {
+                    if (userLogueado.Rol_Usuario == 1)
+                        return RedirectToAction("vInicioAdministrador", "Administrador");
+                    else if (userLogueado.Rol_Usuario == 2)
+                        return RedirectToAction("vInicioVendedor", "Vendedor");
+                }
+            }
+            return RedirectToAction("vNuevoProducto", "NuevoProducto");
+        }
+    }
+}
---
+++ Proyecto2/Proyecto2.ClienteWeb/Controllers/NuevoTipoCategoriaController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Newtonsoft.Json;
+using Proyecto2.ClienteWeb.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto2.ClienteWeb.Controllers
+{
+    public class NuevoTipoCategoriaController : Controller
+    {
+        // GET: NuevoTipoCategoria
+        public ActionResult vNuevoTipoCategoria()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult creandoNuevoTipoCategoria(string nombre)
+        {
+            Usuario userLogueado = Session["USUARIO"] as Usuario;
+            var url = "http://localhost:61291/api/NuevoTipoCategoria?";
+            string action = string.Format("nombre={0}", nombre);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url + action);
+            HttpResponseMessage response = HttpInstance.GetHttpClientInstance().SendAsync(request).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                var responsecontent = response.Content.ReadAsStringAsync().Result;
+                var agregado = JsonConvert.DeserializeObject<Boolean>(responsecontent.ToString());
+                if (agregado)
+                {
+                    if(userLogueado.Rol_Usuario == 1)
+                        return RedirectToAction("vInicioAdministrador", "Administrador");
+                    else if(userLogueado.Rol_Usuario == 2)
+                        return RedirectToAction("vInicioVendedor", "Vendedor");
+                }
+            }
+            return RedirectToAction("vNuevoTipoCategoria", "NuevoTipoCategoria");
+        }
+
+    }
+}
