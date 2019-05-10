29409de8e492894b6d0e39f9c0b3dad0afab3a5c
Ever <everchicasp@gmail.com>
2019-05-10 06:43:06 -0600

    cambios en master

--- Proyecto2/Proyecto2.WebApi/Controllers/MovimientosController.cs
+++ Proyecto2/Proyecto2.WebApi/Controllers/MovimientosController.cs
@@ -35,5 +35,31 @@ namespace Proyecto2.WebApi.Controllers
             conection.Close();
             return lista;
         }
+
+        [HttpPost]
+        public IEnumerable<Movimiento> MovimientosFecha(string fecha)
+        {
+            List<Movimiento> lista = new List<Movimiento>();
+
+            MySqlConnection conection = new MySqlConnection(Conexion.CadenaConexion());
+            conection.Open();
+            MySqlCommand command = new MySqlCommand("LISTA_MOVIMIENTOS", conection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                lista.Add(new Movimiento(
+                    int.Parse(reader.GetValue(0).ToString()),
+                    DateTime.Parse(reader.GetValue(1).ToString()),
+                    reader.GetValue(2).ToString(),
+                    double.Parse(reader.GetValue(3).ToString()),
+                    double.Parse(reader.GetValue(4).ToString()),
+                    int.Parse(reader.GetValue(5).ToString()),
+                    int.Parse(reader.GetValue(6).ToString())));
+            }
+            conection.Close();
+            return lista;
+        }
+
     }
 }
